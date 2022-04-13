3a61f4f54bc9dcd2354ecad48918123ef442d7da
ugocode <obiohaugo5@gmail.com>
2022-04-13 12:40:20 +0100

    find hospitals screen added

---
+++ coronavirus_app/lib/screens/hospitals.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HospitalsScreen extends StatefulWidget {
+  const HospitalsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HospitalsScreen> createState() => _HospitalsScreenState();
+}
+
+class _HospitalsScreenState extends State<HospitalsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(),
+    );
+  }
+}
